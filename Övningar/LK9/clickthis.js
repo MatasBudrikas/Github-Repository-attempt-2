4c05fa547496bb70d8b571e13dd247ea1e33e3c4
MatasBudrikas <54768840+MatasBudrikas@users.noreply.github.com>
2019-09-12 11:36:20 +0200

    update
    11:36

---
+++ Övningar/LK9/clickthis.js
@@ -0,0 +1,13 @@
+function togglePassword() {
+    const input = document.getElementById("password-field");
+
+    switch (input.type) {
+        case "password": {
+            input.type = "text";
+            break;
+        }
+        case "text": {
+
+        }
+    }
+}
