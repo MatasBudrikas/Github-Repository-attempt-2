37734ce91ed39af415ad053f7a6a2bd893a10a48
MatasBudrikas <54768840+MatasBudrikas@users.noreply.github.com>
2019-09-17 10:37:51 +0200

    Major update to project

--- Övningar/LK9/clickthis.js
+++ Övningar/LK9/clickthis.js
@@ -1,5 +1,23 @@
+
+//var count1 = 1
+//function setColor(id, color) {
+// const p = document.getElementById(id);
+// p.style.color = color;
+// if (count1 == 0) {
+//     property.style.backgroundColor = gold;
+//     count1 = 1;
+// }
+
+// if (count1 == 1) {
+//     count1 = 2;
+// }
+// else {
+//     count1 = 0;
+// }
+//}
+
 function togglePassword() {
-    const input = document.getElementById("password-field");
+    var input = document.getElementById("PSWTXT");
 
     switch (input.type) {
         case "password": {
@@ -7,7 +25,31 @@ function togglePassword() {
             break;
         }
         case "text": {
-
+            input.type = "password";
+            break;
+        }
+        default: {
+            console.error("Unknown");
+            break;
         }
     }
 }
+
+function setColor(id, color) {
+    const p = document.getElementById(id);
+    p.style.color = color;
+}
+
+function multibutton() {
+    var button = document.getElementById("RGBswitch");
+    switch (col.back.btnname) {
+        case "red":
+            col.back = Color.White;
+            break;
+    }
+
+}
+
+let t1 = document.getElementById("text1");
+t1.style.color = "darkblue";
+
